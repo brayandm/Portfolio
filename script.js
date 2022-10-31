86cd88072a9eccf82ab010e42ea7f0510ea11172
Brayan Duran <brayan.duran@leaguesofcode.com>
2022-10-31 09:37:56 +0100

    New update

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-window.onscroll = function() {myFunction()};
 mainButton = document.querySelector(".main-button");
+logo = document.querySelector(".logo");
+body = document.querySelector("body");
+header = document.querySelector("header");
+content = document.querySelector(".content");
+delayLogo = 6000;
+
+document.documentElement.style.setProperty("--delay-logo", delayLogo + "ms");
+
+backgroundColor = getComputedStyle(document.documentElement).getPropertyValue("--color-palette-background");
+
+funcLogo();
+
+function funcLogo()
+{
+    setTimeout(function(){ 
+        logo.classList.add("hide");
+        body.style.backgroundColor = backgroundColor;
+        setTimeout(function(){ 
+          header.classList.remove("hide");
+          content.classList.remove("hide");
+          body.style.height = "5000px";
+          window.onscroll = function() {myFunction()};
+      }, 500);
+        
+    }, delayLogo);  
+}
 
 mainButton.addEventListener('click', _ => {
     location.reload();
